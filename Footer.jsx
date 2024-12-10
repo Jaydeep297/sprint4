803f9324fc70eb50b2345cc2d5b95b25f9e36a06
Jaydeep297 <jaydeep2k01@gmail.com>
2024-12-10 02:54:49 -0500

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center py-6 bg-black text-white border-t border-gray-800">
+      <div className="text-center">
+        <p className="text-sm leading-loose text-muted-foreground">
+          Designed and developed by Group 7. © All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
